41caa4a59e34d3477cdfb5fbbf3704a6baae8698
ChristianDantasGermano <christian.dantas.germano@gmail.com>
2021-05-24 17:10:51 -0300

    Ajuste gerais

--- Aplicacao/src/App.js
+++ Aplicacao/src/App.js
@@ -1,7 +1,10 @@
-import Routes from "./controller/routes";
+import './App.css';
+import React from 'react';
+import Routes from './controller/routes';
 
 function App() {
   return (
+
     <div>
       <Routes />
     </div>
